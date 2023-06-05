4de5f3a24934e96f020981e0951162a349b829fb
Yoden <anniw544@hsd.k12.or.us>
2023-06-05 15:03:57 -0700

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,52 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "smallIndustrial1":return img`
+bbbbbbbbbbbbbbbbbbbbbbbb
+bddddddddddddddddddddddb
+bdbbbbbbbbbbbbbbbbbbbbdb
+bbbbbbbbbbbbbbbbbbbbbbbb
+cbcddcbbbbbbbbbbbbcddcbc
+cbcbbcbbbbbbbbbbbbcbbcbc
+cbccccbbbbbbbbbbbbccccbc
+cbbbbbbbbbbbbbbbbbbbbbbc
+cbbbbbbbbbbbbbbbbbbbbbbc
+cbbbbbbbbbbbbbbbbbbbbbbc
+cbbbbbbbbbbbbbbbbbbbbbbc
+cbbbbbbbbbbbbbbbbbbbbbbc
+cbbbbbbbbbbbbbbbbbbbbbbc
+cbbbbbbbbbbbbbbbbbbbbbbc
+cbbbbbbbbbbbbbbbbbbbbbbc
+cbbbbbbbbbbbbbbbbbbbbbbc
+cbbbbbbbbbbbbbbbbbbbbbbc
+cbbbbbbbbbbbbbbbbbbbbbbc
+cbcddcbbbbbbbbbbbbcddcbc
+cbcbbcbbbbbbbbbbbbcbbcbc
+cbccccbbbbbbbbbbbbccccbc
+ccbbbbbbbbbbbbbbbbbbbbcc
+cccccccccccccccccccccccc
+cccccccccccccccccccccccc
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
---
+++ main.ts
@@ -0,0 +1,5 @@
+namespace SpriteKind {
+    export const Wall = SpriteKind.create()
+}
+tiles.setCurrentTilemap(tilemap`level`)
+scene.setBackgroundColor(13)
