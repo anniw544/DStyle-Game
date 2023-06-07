1e1b8a7bcae64b21adaf36639ac6a6ef372c8105
Yoden <anniw544@hsd.k12.or.us>
2023-06-07 10:21:00 -0700

    attempted to make gun

--- main.ts
+++ main.ts
@@ -1,8 +1,52 @@
 namespace SpriteKind {
     export const Wall = SpriteKind.create()
 }
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    let py = 0
+    let px = 0
+    PPB = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . 3 3 3 . . . . . . . 
+        . . . . . 3 3 2 3 3 . . . . . . 
+        . . . . . 3 2 3 2 3 . . . . . . 
+        . . . . . 3 3 2 3 3 . . . . . . 
+        . . . . . . 3 3 3 . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Projectile)
+    PPB.setPosition(px, py)
+})
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     Render.toggleViewMode()
 })
+let PPB: Sprite = null
 tiles.setCurrentTilemap(tilemap`level 1`)
 scene.setBackgroundImage(assets.image`C_F`)
+let mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+let e1 = sprites.create(assets.image`myImage`, SpriteKind.Enemy)
+info.setLife(3)
