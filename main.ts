7267c20f180af688f97c954fae1eb961ab229eaa
Matthew Ertman <113724461+pr0gamr@users.noreply.github.com>
2023-06-15 11:14:53 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,32 +2,49 @@ namespace SpriteKind {
     export const Wall = SpriteKind.create()
 }
 controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
-    projectile = sprites.createProjectileFromSprite(img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . 2 2 . . . . . . . 
-        . . . . . . . 2 2 . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        `, Render.getRenderSpriteInstance(), Render.getAttribute(Render.attribute.dirX), Render.getAttribute(Render.attribute.dirX))
+    if (WGun == true) {
+        WGun = false
+    } else {
+        WGun = true
+    }
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (WGun == true) {
+        projectile = sprites.createProjectileFromSprite(img`
+            2 2 
+            2 2 
+            `, Render.getRenderSpriteInstance(), Render.getAttribute(Render.attribute.dirX) * 50, Render.getAttribute(Render.attribute.dirY) * 50)
+    } else if (WGun == false) {
+        projectile = sprites.createProjectileFromSprite(img`
+            2 2 
+            2 2 
+            `, Render.getRenderSpriteInstance(), Render.getAttribute(Render.attribute.dirX) * 50 + Render.getAttribute(Render.attribute.dirX) * randint(-20, 20), Render.getAttribute(Render.attribute.dirY) * 50 + Render.getAttribute(Render.attribute.dirY) * randint(-20, 20))
+        projectile = sprites.createProjectileFromSprite(img`
+            2 2 
+            2 2 
+            `, Render.getRenderSpriteInstance(), Render.getAttribute(Render.attribute.dirX) * 50 + Render.getAttribute(Render.attribute.dirX) * randint(-20, 20), Render.getAttribute(Render.attribute.dirY) * 50 + Render.getAttribute(Render.attribute.dirY) * randint(-20, 20))
+        projectile = sprites.createProjectileFromSprite(img`
+            2 2 
+            2 2 
+            `, Render.getRenderSpriteInstance(), Render.getAttribute(Render.attribute.dirX) * 50 + Render.getAttribute(Render.attribute.dirX) * randint(-20, 20), Render.getAttribute(Render.attribute.dirY) * 50 + Render.getAttribute(Render.attribute.dirY) * randint(-20, 20))
+        projectile = sprites.createProjectileFromSprite(img`
+            2 2 
+            2 2 
+            `, Render.getRenderSpriteInstance(), Render.getAttribute(Render.attribute.dirX) * 50 + Render.getAttribute(Render.attribute.dirX) * randint(-20, 20), Render.getAttribute(Render.attribute.dirY) * 50 + Render.getAttribute(Render.attribute.dirY) * randint(-20, 20))
+        projectile = sprites.createProjectileFromSprite(img`
+            2 2 
+            2 2 
+            `, Render.getRenderSpriteInstance(), Render.getAttribute(Render.attribute.dirX) * 50 + Render.getAttribute(Render.attribute.dirX) * randint(-20, 20), Render.getAttribute(Render.attribute.dirY) * 50 + Render.getAttribute(Render.attribute.dirY) * randint(-20, 20))
+    }
 })
 controller.menu.onEvent(ControllerButtonEvent.Pressed, function () {
     Render.toggleViewMode()
 })
 let projectile: Sprite = null
+let WGun = false
 scroller.scrollBackgroundWithCamera(scroller.CameraScrollMode.BothDirections, scroller.BackgroundLayer.Layer4)
 tiles.setCurrentTilemap(tilemap`level 1`)
 let e1 = sprites.create(assets.image`myImage`, SpriteKind.Enemy)
 info.setLife(3)
 Render.moveWithController(2, 3, 1)
-let WGun = 1
+WGun = true
